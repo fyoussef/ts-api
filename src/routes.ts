627181e5556f9dce162041e4ecf601b22e875b1a
fyoussef <fillipyuou@gmail.com>
2022-12-08 20:00:29 -0300

    :rocket: implementing TDD pratices to development of routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { prisma } from "./db/prismaClient";
 
 const routes = Router();
@@ -10,6 +9,54 @@ routes.get("/", (req, res) => {
   });
 });
 
+function validateBarberId(req: Request, res: Response, next: NextFunction) {
+  const id = req.params.id;
+  if (!id) {
+    throw new Error("O id é obrigatório");
+  }
+  next();
+}
+
+routes.get("/barber/:id", validateBarberId, async (req, res) => {
+  const id = req.params?.id;
+
+  const barber = await prisma.barber.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  res.json({
+    barber,
+  });
+});
+
+routes.post("/barber", async (req, res) => {
+  const { name } = req.body;
+
+  const barber = await prisma.barber.create({
+    data: {
+      name,
+    },
+  });
+
+  res.json({
+    barber,
+  });
+});
+
+routes.delete("/barber/:id", validateBarberId, async (req, res) => {
+  const id = req.params.id;
+
+  const barber = await prisma.barber.delete({
+    where: {
+      id,
+    },
+  });
+
+  res.json(barber);
+});
+
 routes.get("/schedules", async (req, res) => {
   const schedules = await prisma.barber_Schedules.findMany();
   res.json({
@@ -17,30 +64,52 @@ routes.get("/schedules", async (req, res) => {
   });
 });
 
-routes.get("/barber/:barber", async (req, res) => {
-  res.json({
-    param: req.params,
+routes.post("/schedules", async (req, res) => {
+  const { barber_id } = req.body;
+
+  const hours = [
+    { barber_id, hour: "09:00" },
+    { barber_id, hour: "09:30" },
+    { barber_id, hour: "10:00" },
+    { barber_id, hour: "10:30" },
+    { barber_id, hour: "11:00" },
+    { barber_id, hour: "11:30" },
+    { barber_id, hour: "12:00" },
+    { barber_id, hour: "12:30" },
+    { barber_id, hour: "13:00" },
+    { barber_id, hour: "13:30" },
+    { barber_id, hour: "14:00" },
+    { barber_id, hour: "14:30" },
+    { barber_id, hour: "15:00" },
+    { barber_id, hour: "15:30" },
+    { barber_id, hour: "16:00" },
+    { barber_id, hour: "16:30" },
+    { barber_id, hour: "17:00" },
+    { barber_id, hour: "17:30" },
+    { barber_id, hour: "18:00" },
+    { barber_id, hour: "18:30" },
+    { barber_id, hour: "19:00" },
+  ];
+
+  const schedules = await prisma.barber_Schedules.createMany({
+    data: hours,
   });
+
+  return res.json(schedules);
 });
 
-routes.post("/barber", async (req, res) => {
-  const { name } = req.body;
-  const barber = await prisma.barber.create({
-    data: {
-      name,
+routes.delete("/schedules/:id", validateBarberId, async (req, res) => {
+  const id = req.params.id;
+
+  const deleted = await prisma.barber_Schedules.deleteMany({
+    where: {
+      barber_id: {
+        equals: id,
+      },
     },
   });
-  res.json({
-    barber,
-  });
-});
 
-/* routes.post("/schedules", async (req, res) => {
-  await prisma.barber_Schedules.createMany({
-    data: [
-      {}
-    ]
-  })
-}); */
+  res.json(deleted);
+});
 
 export { routes };
--- test/API.test.ts
+++ test/API.test.ts
@@ -1,20 +1,63 @@
 import { api } from "../src/services/axios";
 
-test("Should test API", async () => {
+test("should test API", async () => {
   // test API connection
   const { status } = await api.get("/");
 
   expect(status).toBe(200);
+});
 
-  // test create barber
-  const { data } = await api.post("/barber", {
-    name: "Filipi",
+describe("should test barber routes", () => {
+  var barber_id: string;
+  it("should create barber", async () => {
+    // test create barber
+    const { data } = await api.post("/barber", {
+      name: "Filipi",
+    });
+    const barber = data.barber;
+    expect(barber).not.toBeUndefined();
+    barber_id = barber.id;
   });
 
-  expect(data.barber).toHaveProperty("name");
+  it("should get barber", async () => {
+    const { data } = await api.get("/barber/" + barber_id);
+    const barberData = data.barber;
 
-  // test if barber schedules were created
-  /* const req = await api.get("/schedules");
-  const { schedules } = req.data;
-  expect(schedules).toHaveLength(20); */
+    expect(barberData).not.toBeUndefined();
+  });
+
+  it("should delete barber", async () => {
+    const { data } = await api.delete("/barber/" + barber_id);
+
+    expect(data).not.toBeUndefined();
+  });
+});
+
+describe("should test schedules routes", () => {
+  var barber_id: string;
+  it("should create schedule to one barber", async () => {
+    // create barber
+    const { data } = await api.post("/barber", {
+      name: "Filipi",
+    });
+    const barber = data.barber;
+    barber_id = barber.id;
+
+    const { data: schedulesCreated } = await api.post("/schedules", {
+      barber_id: barber.id,
+    });
+    const count = schedulesCreated.count;
+
+    expect(count).toBe(21);
+  });
+
+  it("should delete all schedules of barber", async () => {
+    const { data: deletedData } = await api.delete("/schedules/" + barber_id);
+    const count = deletedData.count;
+
+    expect(count).toBe(21);
+
+    // delete barber
+    await api.delete("/barber/" + barber_id);
+  });
 });
