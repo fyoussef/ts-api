03077f2f6ef2d41e68486e7266d7482310ff722f
fyoussef <filipyuou@gmail.com>
2023-01-16 20:12:41 -0300

    :sparkles: add test to get barber schedules

--- test/routes/schedule.spec.ts
+++ test/routes/schedule.spec.ts
@@ -1,9 +1,10 @@
 import { api } from "../../src/utils/axios"
 
 describe('Test schedule route', () => {
+  let barber_id = '08b154b3-4b41-413e-8b9d-88bd7891fa15'
   it('should create an schedule to barber', async () => {
     const { data, status } = await api.post('/schedules', {
-      barber_id: '08b154b3-4b41-413e-8b9d-88bd7891fa15',
+      barber_id,
       start_hour: '09:00',
       end_hour: '19:00',
       interval: 30
@@ -14,5 +15,11 @@ describe('Test schedule route', () => {
     expect(data).not.toBeUndefined()
     expect(data.msg).not.toBeUndefined()
     expect(data.msg).toBe('Barber schedule created')
+  })
+
+  it('should get barber schedules', async () => {
+    const { data } = await api.get('/schedules/' + barber_id)
+
+    expect(data).toHaveProperty('schedules')
   })
 })
