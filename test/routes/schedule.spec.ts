a0c5c299795e7fd5e774430379c0874ad8dcd265
fyoussef <filipyuou@gmail.com>
2023-01-15 21:20:14 -0300

    adding test to create barber schedule

--- test/routes/schedule.spec.ts
+++ test/routes/schedule.spec.ts
@@ -2,11 +2,17 @@ import { api } from "../../src/utils/axios"
 
 describe('Test schedule route', () => {
   it('should create an schedule to barber', async () => {
-    const { data } = await api.post('/schedules', {
+    const { data, status } = await api.post('/schedules', {
       barber_id: '08b154b3-4b41-413e-8b9d-88bd7891fa15',
       start_hour: '09:00',
       end_hour: '19:00',
       interval: 30
     })
+
+    expect(status).toBeGreaterThanOrEqual(200)
+    expect(status).toBeLessThan(300)
+    expect(data).not.toBeUndefined()
+    expect(data.msg).not.toBeUndefined()
+    expect(data.msg).toBe('Barber schedule created')
   })
 })
