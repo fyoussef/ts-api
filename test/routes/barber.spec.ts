62beb8ed979e45ebea7885d89ce8c3761d8f65f5
fyoussef <filipyuou@gmail.com>
2023-01-17 21:51:09 -0300

    descomment tests

--- test/routes/barber.spec.ts
+++ test/routes/barber.spec.ts
@@ -1,55 +1,55 @@
-// import { Barber } from "@prisma/client";
-// import { api } from "../../src/utils/axios"
+import { Barber } from "@prisma/client";
+import { api } from "../../src/utils/axios"
 
-// describe('Test route to create barber', () => {
-//   let barber: Barber;
+describe('Test route to create barber', () => {
+  let barber: Barber;
   
-//   it('should create barber', async () => {
-//     const { data } = await api.post('/barber', {name: 'Youssef'})
+  it('should create barber', async () => {
+    const { data } = await api.post('/barber', {name: 'Youssef'})
 
-//     barber = data
+    barber = data
     
-//     expect(barber).not.toBeUndefined()
-//     expect(barber).toHaveProperty('id')
-//     expect(barber).toHaveProperty('name')
-//     expect(barber.name).toEqual('Youssef')
-//   })
+    expect(barber).not.toBeUndefined()
+    expect(barber).toHaveProperty('id')
+    expect(barber).toHaveProperty('name')
+    expect(barber.name).toEqual('Youssef')
+  })
 
-//   it('shold get barber', async () => {
-//     const { data: barberFound } = await api.get('/barber/' + barber.id)
+  it('shold get barber', async () => {
+    const { data: barberFound } = await api.get('/barber/' + barber.id)
 
-//     expect(barberFound).not.toBeUndefined()
-//     expect(barberFound).toHaveProperty('id')
-//     expect(barberFound).toHaveProperty('name')
-//   })
+    expect(barberFound).not.toBeUndefined()
+    expect(barberFound).toHaveProperty('id')
+    expect(barberFound).toHaveProperty('name')
+  })
 
-//   it('should get barbers', async () => {
-//     const { data } = await api.get('/barber')
-//     const barbers = data
+  it('should get barbers', async () => {
+    const { data } = await api.get('/barber')
+    const barbers = data
 
-//     expect(barbers).not.toBeUndefined()
-//     expect(barbers).toBeTruthy()
-//   })
+    expect(barbers).not.toBeUndefined()
+    expect(barbers).toBeTruthy()
+  })
 
-//   it('shoud update barber', async () => {
+  it('shoud update barber', async () => {
 
-//     const { data } = await api.put('/barber/' + barber.id, {
-//       name: 'Filipi'
-//     })
-//     const barberUpdated = data.barber
+    const { data } = await api.put('/barber/' + barber.id, {
+      name: 'Filipi'
+    })
+    const barberUpdated = data.barber
 
-//     expect(barberUpdated).not.toBeUndefined()
-//     expect(barberUpdated).toHaveProperty('id')
-//     expect(barberUpdated).toHaveProperty('name')
-//     expect(barberUpdated.name).toBe('Filipi')
-//   })
+    expect(barberUpdated).not.toBeUndefined()
+    expect(barberUpdated).toHaveProperty('id')
+    expect(barberUpdated).toHaveProperty('name')
+    expect(barberUpdated.name).toBe('Filipi')
+  })
 
-//   it('should delete barber', async () => {
-//     const res = await api.delete('/barber/' + barber.id)
-//     const { status, data } = res
+  it('should delete barber', async () => {
+    const res = await api.delete('/barber/' + barber.id)
+    const { status, data } = res
 
-//     expect(data.msg).toBe('Barber has been deleted')
-//     expect(status).toBeGreaterThan(200);
-//     expect(status).toBeLessThan(300)
-//   })
-// })
+    expect(data.msg).toBe('Barber has been deleted')
+    expect(status).toBeGreaterThan(200);
+    expect(status).toBeLessThan(300)
+  })
+})
--- test/routes/schedule.spec.ts
+++ test/routes/schedule.spec.ts
@@ -1,41 +1,41 @@
-// import { api } from "../../src/utils/axios"
+import { api } from "../../src/utils/axios"
 
-// describe('Test schedule route', () => {
-//   let barber_id = 'a92d4389-8a32-4a63-8340-a626da4fe430'
+describe('Test schedule route', () => {
+  let barber_id = 'a92d4389-8a32-4a63-8340-a626da4fe430'
 
-//   it('should create an schedule to barber', async () => {
+  it('should create an schedule to barber', async () => {
 
-//     const { data: barberData } = await api.get('/barber')
-//     barber_id = barberData[0].id
+    const { data: barberData } = await api.get('/barber')
+    barber_id = barberData[0].id
 
-//     const { data, status } = await api.post('/schedules', {
-//       barber_id,
-//       start_hour: '09:00',
-//       end_hour: '19:00',
-//       interval: 30
-//     })
+    const { data, status } = await api.post('/schedules', {
+      barber_id,
+      start_hour: '09:00',
+      end_hour: '19:00',
+      interval: 30
+    })
 
-//     expect(status).toBeGreaterThanOrEqual(200)
-//     expect(status).toBeLessThan(300)
-//     expect(data).not.toBeUndefined()
-//     expect(data.msg).not.toBeUndefined()
-//     expect(data.msg).toBe('Barber schedule created')
-//   })
+    expect(status).toBeGreaterThanOrEqual(200)
+    expect(status).toBeLessThan(300)
+    expect(data).not.toBeUndefined()
+    expect(data.msg).not.toBeUndefined()
+    expect(data.msg).toBe('Barber schedule created')
+  })
 
-//   it('should get barber schedules', async () => {
-//     const { data } = await api.get('/schedules/' + barber_id)
+  it('should get barber schedules', async () => {
+    const { data } = await api.get('/schedules/' + barber_id)
 
-//     expect(data).toHaveProperty('schedules')
-//   })
+    expect(data).toHaveProperty('schedules')
+  })
 
-//   it('should delete barber schedules', async () => {
+  it('should delete barber schedules', async () => {
 
-//     const {data, status} = await api.delete('/schedules/' + barber_id)
+    const {data, status} = await api.delete('/schedules/' + barber_id)
 
-//     expect(data).toHaveProperty('msg')
-//     expect(data.msg).toBe('Barber schedule has been deleted')
-//     expect(status).toBeGreaterThanOrEqual(200);
-//     expect(status).toBeLessThan(300)
+    expect(data).toHaveProperty('msg')
+    expect(data.msg).toBe('Barber schedule has been deleted')
+    expect(status).toBeGreaterThanOrEqual(200);
+    expect(status).toBeLessThan(300)
 
-//   })
-// })
+  })
+})
