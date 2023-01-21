b76eea3b4638c4527b255bbf4330365eb9021ff0
fyoussef <filipyuou@gmail.com>
2023-01-21 08:32:50 -0300

    add validations mathod

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,9 +1,17 @@
-import { UserDTO } from "../../dto/user/user-dto";
+import { UserDTO } from '../../dto/user/user-dto'
+import { UserValidation } from '../validations/user'
 
 export class User {
-  constructor(
-    private userDTO: UserDTO
-  ) {}
+  constructor(private userDTO: UserDTO) {}
 
-  validate() {}
+  validate() {
+    const validator = new UserValidation(this.userDTO)
+
+    return (
+      validator.validateName() &&
+      validator.validateEmail() &&
+      validator.validatePassword() &&
+      validator.validatePhone()
+    )
+  }
 }
