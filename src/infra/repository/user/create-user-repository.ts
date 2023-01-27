d39681cd97777a23f8ba624554bff707b87997b5
fyoussef <fillipyuou@gmail.com>
2023-01-27 17:27:47 -0300

    handling no register if email already exists

--- src/infra/repository/user/create-user-repository.ts
+++ src/infra/repository/user/create-user-repository.ts
@@ -1,16 +1,38 @@
-import { PrismaClient } from "@prisma/client";
-import { CreateUserContract, UserOmitPassword } from "../../../domain/contracts/user/create-user-contract";
-import { UserDTO } from "../../../dto/user/user-dto";
+import { PrismaClient } from '@prisma/client'
+import {
+  CreateUserContract,
+  UserOmitPassword
+} from '../../../domain/contracts/user/create-user-contract'
+import { UserDTO } from '../../../dto/user/user-dto'
+import { HttpResponse } from '../../../utils/helpers/http-response'
 
 export class CreateUserRepository implements CreateUserContract {
-  constructor (
-    private readonly prisma: PrismaClient
-  ) {}
+  constructor(private readonly prisma: PrismaClient) {}
 
-  async create({ email, name, password, phone }: UserDTO): Promise<UserOmitPassword> {
+  async create({
+    email,
+    name,
+    password,
+    phone
+  }: UserDTO): Promise<UserOmitPassword> {
+    const userAlreadyExists = await this.prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
+
+    if (userAlreadyExists) {
+      throw new HttpResponse().badRequest(
+        new Error('Esta email não está indisponível')
+      )
+    }
+
     const user = await this.prisma.user.create({
       data: {
-        email, name, password, phone
+        email,
+        name,
+        password,
+        phone
       },
       select: {
         email: true,
@@ -22,5 +44,4 @@ export class CreateUserRepository implements CreateUserContract {
 
     return user
   }
-
 }
--- src/server.ts
+++ src/server.ts
@@ -9,17 +9,4 @@ app.use(cors())
 
 app.use(routes)
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    return res.status(400).json({
-      message: err.message
-    })
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error'
-  })
-})
-
 app.listen(5000, () => console.log('Server running on port :5000'))
